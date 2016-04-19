51119ac52e55006dc6a20b24b1d6bfa7f4e780bd
ppapasaikas <panp80@gmail.com>
2016-04-19 18:11:26 +0200

    change file mode in lib

--- lib/bedtools_intersect.sh
+++ lib/bedtools_intersect.sh
@@ -5,5 +5,5 @@ out_intr_segm_sorted=$2
 no_junct_reads=$3
 output=$4
 
-if [ "$rnaseq" = "U" ]; then bedtools intersect -c -sorted -a $out_intr_segm_sorted -b $no_junct_reads > $output; fi
-if [ "$rnaseq" = "S" ]; then bedtools intersect -c -s -sorted -a $out_intr_segm_sorted -b $no_junct_reads > $output; fi
+if [ "$rnaseq" = "U" ]; then bedtools intersect -c -F 0.25 -f 0.5 -e -split -sorted -a $out_intr_segm_sorted -b $no_junct_reads > $output; fi
+if [ "$rnaseq" = "S" ]; then bedtools intersect -c -F 0.25 -f 0.5 -e -split -s -sorted -a $out_intr_segm_sorted -b $no_junct_reads > $output; fi
