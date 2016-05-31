17544d86d56ffd488129c4c8de1048615ba29a5a
Andre Gohr <andre.gohr@crg.eu>
2016-05-31 09:41:58 +0200

    change from using temp file to inline pipeing in sort_wrapper

--- lib/sort_wrapper.sh
+++ lib/sort_wrapper.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-
 # $1 input_bed
 # $2 bam
 # $3 output_bed
 
-#sort -k1,1 -k2,2n $1 > $2
-
-samtools view -H $2 | grep SQ | cut -f 2 | awk '{ sub(/^SN:/, ""); print;}' > names_bam1_tempfile_87askjhmsjx6542.txt
-bedtools sort -faidx names_bam1_tempfile_87askjhmsjx6542.txt -i $1 > $3 
-rm names_bam1_tempfile_87askjhmsjx6542.txt
-
-#sleep 1m
+bedtools sort -faidx <(samtools view -H $2 | grep SQ | cut -f 2 | awk '{ sub(/^SN:/, ""); print;}') -i $1 > $3
